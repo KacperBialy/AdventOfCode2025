452c7c44779eef1298fb49f3c2f2e0caca5f24be
Kacper Bialy <kacper1400@o2.pl>
2025-12-16 00:48:51 +0100

    Day1 - Part1

---
+++ AoC2025/Day1.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC2025;
+
+public class Day1
+{
+    private const char LeftDirection = 'L';
+    private const char RightDirection = 'R';
+
+    public static (int endpoint, int zeroEntrance) CalculateDialPosition(string[] input, int startingDial)
+    {
+        var zeroEntrance = 0;
+        foreach (var row in input)
+        {
+            var direction = row[0];
+            var value = int.Parse(row[1..]);
+
+            startingDial = direction switch
+            {
+                LeftDirection => ((startingDial - value) % 100 + 100) % 100,
+                _ => (startingDial + value) % 100
+            };
+
+            if (startingDial == 0)
+                zeroEntrance++;
+        }
+
+        return (startingDial, zeroEntrance);
+    }
+}
---
+++ AoC2025Tests/Day1Tests.cs
@@ -0,0 +1,66 @@
+﻿using AoC2025;
+using FluentAssertions;
+
+namespace TestProject1;
+
+public class Day1Tests
+{
+    [Fact]
+    public void CalculateDialPosition_LeftMove_WithoutFullRotation_ReturnsCorrectDialPosition()
+    {
+        string[] input = ["L45"];
+
+        var result = Day1.CalculateDialPosition(input, 50);
+
+        result.endpoint.Should().Be(5);
+    }
+
+    [Fact]
+    public void CalculateDialPosition_LeftMove_WithFullRotation_ReturnsCorrectDialPosition()
+    {
+        string[] input = ["L420"];
+
+        var result = Day1.CalculateDialPosition(input, 43);
+
+        result.endpoint.Should().Be(23);
+    }
+
+    [Fact]
+    public void CalculateDialPosition_LeftRight_WithoutFullRotation_ReturnsCorrectDialPosition()
+    {
+        string[] input = ["R118"];
+
+        var result = Day1.CalculateDialPosition(input, 50);
+
+        result.endpoint.Should().Be(68);
+    }
+
+    [Fact]
+    public void CalculateDialPosition_LeftRight_WithFullRotation_ReturnsCorrectDialPosition()
+    {
+        string[] input = ["R68"];
+
+        var result = Day1.CalculateDialPosition(input, 50);
+
+        result.endpoint.Should().Be(18);
+    }
+
+    [Fact]
+    public void CalculateDialPosition_WhenComplex_ReturnsCorrectDialPositionAndZeroEntrance()
+    {
+        string[] input = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
+
+        var result = Day1.CalculateDialPosition(input, 50);
+
+        result.endpoint.Should().Be(32);
+        result.zeroEntrance.Should().Be(6);
+    }
+    
+    [Fact]
+    public void CalculateDialPosition_WhenDay1Input_ReturnsEntrance()
+    {
+        var input = File.ReadAllLines("Day1/Day1.txt");
+
+        var result = Day1.CalculateDialPosition(input, 50);
+    }
+}
