f6da5ab1e644335ccc35d5c84f2b6fb25f4fb523
Kacper Bialy <kacper1400@o2.pl>
2025-12-19 22:19:49 +0100

    Day3 - Part1

---
+++ AoC2025/Day3.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC2025;
+
+public class Day3
+{
+    public static ulong SumTotalJoltage(string[] banks)
+    {
+        ulong sum = 0;
+        foreach (var bank in banks)
+        {
+            var leftMax = ulong.MinValue;
+            var rightMax = ulong.MinValue;
+
+            for (var i = 0; i < bank.Length - 1; i++)
+            {
+                var left = ulong.Parse(bank[i].ToString());
+                var right = ulong.Parse(bank[i + 1].ToString());
+
+                if (left > leftMax)
+                {
+                    leftMax = left;
+                    rightMax = right;
+                }
+
+                if (right > rightMax)
+                    rightMax = right;
+            }
+
+            sum += ulong.Parse(leftMax.ToString() + rightMax);
+        }
+
+        return sum;
+    }
+}
---
+++ AoC2025Tests/Day3Tests.cs
@@ -0,0 +1,30 @@
+﻿using AoC2025;
+using FluentAssertions;
+
+namespace TestProject1;
+
+public class Day3Tests
+{
+
+    [Theory]
+    [InlineData("987654321111111", 98)]
+    [InlineData("811111111111119", 89)]
+    [InlineData("234234234234278", 78)]
+    [InlineData("818181911112111", 92)]
+    public void SumTotalJoltage_WhenOneBank_ReturnsCorrectSum(string bank, ulong expectedSum)
+    {
+        var sum = Day3.SumTotalJoltage([bank]);
+
+        sum.Should().Be(expectedSum);
+    }
+    
+    [Fact]
+    public void SumTotalJoltage_WhenPart1Input_ReturnsCorrectSum()
+    {
+        var banks = File.ReadAllLines("Inputs/Day3/Day3.txt");
+        
+        var sum = Day3.SumTotalJoltage(banks);
+        
+        sum.Should().Be(0);
+    }
+}
