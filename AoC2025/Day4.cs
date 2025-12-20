9ccffc7daf48b3e09d1bab6dccf1b62a85a7f43a
Kacper Bialy <kacper1400@o2.pl>
2025-12-20 22:47:34 +0100

    Day4 - Part2

--- AoC2025/Day4.cs
+++ AoC2025/Day4.cs
@@ -2,47 +2,79 @@
 
 public class Day4
 {
+    private static readonly (int dx, int dy)[] Moves =
+    {
+        (-1, -1), (-1, 0), (-1, 1),
+        ( 0,  1),
+        ( 1,  1), ( 1, 0), ( 1, -1),
+        ( 0, -1)
+    };
+
+    private static bool InBounds(char[,] grid, int x, int y) =>
+        x >= 0 && y >= 0 &&
+        x < grid.GetLength(0) &&
+        y < grid.GetLength(1);
+
+    private static int CountAdjacent(char[,] grid, int i, int j)
+    {
+        var count = 0;
+
+        foreach (var (dx, dy) in Moves)
+        {
+            var x = i + dx;
+            var y = j + dy;
+
+            if (InBounds(grid, x, y) && grid[x, y] == '@')
+                count++;
+        }
+
+        return count;
+    }
+
     public static int CalculateAccessedRows(char[,] grid)
     {
-        var moves = new List<(int x, int y)>()
-        {
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, -1)
-        };
-
         var sum = 0;
+
         for (var i = 0; i < grid.GetLength(0); i++)
+        for (var j = 0; j < grid.GetLength(1); j++)
         {
+            if (grid[i, j] != '@')
+                continue;
+
+            if (CountAdjacent(grid, i, j) < 4)
+                sum++;
+        }
+
+        return sum;
+    }
+
+    public static int CalculateAccessedRowsPart2(char[,] grid)
+    {
+        var sum = 0;
+
+        while (true)
+        {
+            var changes = 0;
+            var gridCopy = (char[,])grid.Clone();
+
+            for (var i = 0; i < grid.GetLength(0); i++)
             for (var j = 0; j < grid.GetLength(1); j++)
             {
                 if (grid[i, j] != '@')
                     continue;
 
-                var rollsOfPaper = 0;
-                foreach (var move in moves)
+                if (CountAdjacent(grid, i, j) < 4)
                 {
-                    var x = i + move.x;
-                    var y = j + move.y;
-
-                    if (x < 0 || y < 0)
-                        continue;
-
-                    if (x > grid.GetLength(0) - 1 || y > grid.GetLength(1) - 1)
-                        continue;
-
-                    if (grid[x, y] == '@')
-                        rollsOfPaper++;
+                    gridCopy[i, j] = 'x';
+                    changes++;
                 }
-
-                if (rollsOfPaper < 4)
-                    sum++;
             }
+
+            if (changes == 0)
+                break;
+
+            sum += changes;
+            grid = gridCopy;
         }
 
         return sum;
--- AoC2025Tests/Day4Tests.cs
+++ AoC2025Tests/Day4Tests.cs
@@ -45,4 +45,45 @@ public class Day4Tests
 
         result.Should().Be(1604);
     }
+    
+    [Fact]
+    public void CalculateAccessedRows_WhenSimplePart2Input_ReturnsCorrectValue()
+    {
+        char[,] grid =
+        {
+            {'.','.', '@','@','.', '@','@','@','@','.'},
+            {'@','@','@','.', '@','.', '@','.', '@','@'},
+            {'@','@','@','@','@','.', '@','.', '@','@'},
+            {'@','.', '@','@','@','@','.', '.','@','.'},
+            {'@','@','.', '@','@','@','@','.', '@','@'},
+            {'.','@','@','@','@','@','@','@','.', '@'},
+            {'.','@','.', '@','.', '@','.', '@','@','@'},
+            {'@','.', '@','@','@','.', '@','@','@','@'},
+            {'.','@','@','@','@','@','@','@','@','.'},
+            {'@','.', '@','.', '@','@','@','.', '@','.'}
+        };
+
+        var result = Day4.CalculateAccessedRowsPart2(grid);
+
+        result.Should().Be(43);
+    }
+    
+    [Fact]
+    public void CalculateAccessedRows_WhenPart2Input_ReturnsCorrectValue()
+    {
+        var lines = File.ReadAllLines("Inputs/Day4/Day4.txt");
+
+        var rows = lines.Length;
+        var cols = lines[0].Length;
+
+        var grid = new char[rows, cols];
+
+        for (var r = 0; r < rows; r++)
+        for (var c = 0; c < cols; c++)
+            grid[r, c] = lines[r][c];
+
+        var result = Day4.CalculateAccessedRowsPart2(grid);
+
+        result.Should().Be(9397);
+    }
 }
