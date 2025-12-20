72cfaed4298c5fc12b7ac6acc0ae5876246760c5
Kacper Bialy <kacper1400@o2.pl>
2025-12-20 21:58:24 +0100

    Day4 - Part1

---
+++ AoC2025/Day4.cs
@@ -0,0 +1,50 @@
+﻿namespace AoC2025;
+
+public class Day4
+{
+    public static int CalculateAccessedRows(char[,] grid)
+    {
+        var moves = new List<(int x, int y)>()
+        {
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+            (1, 0),
+            (1, -1),
+            (0, -1)
+        };
+
+        var sum = 0;
+        for (var i = 0; i < grid.GetLength(0); i++)
+        {
+            for (var j = 0; j < grid.GetLength(1); j++)
+            {
+                if (grid[i, j] != '@')
+                    continue;
+
+                var rollsOfPaper = 0;
+                foreach (var move in moves)
+                {
+                    var x = i + move.x;
+                    var y = j + move.y;
+
+                    if (x < 0 || y < 0)
+                        continue;
+
+                    if (x > grid.GetLength(0) - 1 || y > grid.GetLength(1) - 1)
+                        continue;
+
+                    if (grid[x, y] == '@')
+                        rollsOfPaper++;
+                }
+
+                if (rollsOfPaper < 4)
+                    sum++;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ AoC2025Tests/Day4Tests.cs
@@ -0,0 +1,48 @@
+﻿using AoC2025;
+using FluentAssertions;
+
+namespace TestProject1;
+
+public class Day4Tests
+{
+    [Fact]
+    public void CalculateAccessedRows_WhenSimplePart1Input_ReturnsCorrectValue()
+    {
+        char[,] grid =
+        {
+            {'.', '.', '@', '@', '.', '@', '@', '@', '@', '.'},
+            {'@', '@', '@', '.', '@', '.', '@', '.', '@', '@'},
+            {'@', '@', '@', '@', '@', '.', '@', '.', '@', '@'},
+            {'@', '.', '@', '@', '@', '@', '.', '.', '@', '.'},
+            {'@', '@', '.', '@', '@', '@', '@', '.', '@', '@'},
+            {'.', '@', '@', '@', '@', '@', '@', '@', '.', '@'},
+            {'.', '@', '.', '@', '.', '@', '.', '@', '@', '@'},
+            {'@', '.', '@', '@', '@', '.', '@', '@', '@', '@'},
+            {'.', '@', '@', '@', '@', '@', '@', '@', '@', '.'},
+            {'@', '.', '@', '.', '.', '@', '@', '@', '.', '@'}
+        };
+
+        var result = Day4.CalculateAccessedRows(grid);
+
+        result.Should().Be(13);
+    }
+    
+    [Fact]
+    public void CalculateAccessedRows_WhenPart1Input_ReturnsCorrectValue()
+    {
+        var lines = File.ReadAllLines("Inputs/Day4/Day4.txt");
+
+        var rows = lines.Length;
+        var cols = lines[0].Length;
+
+        var grid = new char[rows, cols];
+
+        for (var r = 0; r < rows; r++)
+        for (var c = 0; c < cols; c++)
+            grid[r, c] = lines[r][c];
+
+        var result = Day4.CalculateAccessedRows(grid);
+
+        result.Should().Be(1604);
+    }
+}
