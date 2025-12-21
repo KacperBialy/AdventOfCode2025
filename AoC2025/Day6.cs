fc69ad6d583caf9fb052130fd9f29aab395d59d7
Kacper Bialy <kacper1400@o2.pl>
2025-12-21 14:35:25 +0100

    Day6 - Part1

---
+++ AoC2025/Day6.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace AoC2025;
+
+public class Day6
+{
+    public static BigInteger SumOfAllOperations(int[,] numbers, char[] operations)
+    {
+        BigInteger sum = 0;
+        foreach (var (index, operation) in operations.Index())
+        {
+            BigInteger operationSum = 0;
+            if (operation == '*')
+            {
+                operationSum = 1;
+                for (var i = 0; i < numbers.GetLength(0); i++)
+                    operationSum *= numbers[i, index];
+            }
+            else if (operation == '+')
+            {
+                for (var i = 0; i < numbers.GetLength(0); i++)
+                    sum += numbers[i, index];
+            }
+
+            sum += operationSum;
+        }
+
+        return sum;
+    }
+}
---
+++ AoC2025Tests/Day6Tests.cs
@@ -0,0 +1,74 @@
+﻿using AoC2025;
+using FluentAssertions;
+
+namespace TestProject1;
+
+public class Day6Tests
+{
+    [Fact]
+    public void SumOfAllOperations_WhenSimpleInput_ReturnsCorrectSum()
+    {
+        var input = """
+                    123 328  51 64
+                    45 64  387 23
+                    6 98  215 314
+                    *   +   *   +
+                    """;
+        // Split lines and remove empty ones
+        var (numbers, operations) = PrepareData(input);
+
+        var result = Day6.SumOfAllOperations(numbers, operations);
+        
+        result.Should().Be(4277556);
+    }
+    
+    [Fact]
+    public void SumOfAllOperations_WhenPart1Input_ReturnsCorrectSum()
+    {
+        var input = File.ReadAllText("Inputs/Day6/Day6.txt");
+
+        // Split lines and remove empty ones
+        var (numbers, operations) = PrepareData(input);
+
+        var result = Day6.SumOfAllOperations(numbers, operations);
+        
+        result.Should().Be(4277556);
+    }
+
+    private static (int[,] numbers, char[] operations) PrepareData(string input)
+    {
+        var lines = input
+            .Split(Environment.NewLine);
+
+        // Last line contains operations
+        var operationLine = lines[^1];
+        var operations = operationLine
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s[0])
+            .ToArray();
+
+        // Remaining lines contain numbers
+        var numberLines = lines[..^1];
+        var rows = numberLines.Length;
+        var cols = numberLines[0]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Length;
+
+        var numbers = new int[rows, cols];
+
+        for (var i = 0; i < rows; i++)
+        {
+            var values = numberLines[i]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (var j = 0; j < cols; j++)
+            {
+                numbers[i, j] = values[j];
+            }
+        }
+
+        return (numbers, operations);
+    }
+}
