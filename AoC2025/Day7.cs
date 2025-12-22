fbe12e4bb3c8178b56ec62f904b186ea3cc8614a
Kacper Bialy <kacper1400@o2.pl>
2025-12-22 21:55:55 +0100

    Day7 - Part1

---
+++ AoC2025/Day7.cs
@@ -0,0 +1,47 @@
+﻿namespace AoC2025;
+
+public class Day7
+{
+    public static int CalculateNumberOfSplits(char[,] map, (int x, int y) start)
+    {
+        var rows = map.GetLength(0);
+        var cols= map.GetLength(1);
+        
+        var points = new HashSet<(int x, int y)>()
+        {
+            start
+        };
+
+        var sum = 0;
+        var newPoints = new HashSet<(int x, int y)>();
+
+        while (true)
+        {
+            foreach (var point in points)
+            {
+                if(point.x + 1 > rows - 1 || (point.y + 1 > cols - 1 && point.y - 1 > 0))
+                    continue;
+                
+                switch (map[point.x + 1, point.y])
+                {
+                    case '.':
+                        newPoints.Add((point.x + 1, point.y));
+                        break;
+                    case '^':
+                        sum++;
+                        newPoints.Add((point.x + 1 , point.y - 1));
+                        newPoints.Add((point.x + 1, point.y + 1));               
+                        break;
+                }
+            }
+
+            if (newPoints.Count == 0)
+                break;
+
+            points = newPoints;
+            newPoints = [];
+        }
+
+        return sum;
+    }
+}
---
+++ AoC2025Tests/Day7Tests.cs
@@ -0,0 +1,78 @@
+﻿using AoC2025;
+using FluentAssertions;
+
+namespace TestProject1;
+
+public class Day7Tests
+{
+    [Fact]
+    public void CalculateNumberOfSplits_WhenSimpleInput_ReturnsCorrectCount()
+    {
+        var input = """
+                    .......S.......
+                    ...............
+                    .......^.......
+                    ...............
+                    ......^.^......
+                    ...............
+                    .....^.^.^.....
+                    ...............
+                    ....^.^...^....
+                    ...............
+                    ...^.^...^.^...
+                    ...............
+                    ..^...^.....^..
+                    ...............
+                    .^.^.^.^.^...^.
+                    ...............
+                    """;
+
+        var lines = input.Split(Environment.NewLine);
+        var rows = lines.Length;
+        var cols = lines[0].Length;
+
+        var start = (0, 0);
+        var map = new char[rows, cols];
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (lines[i][j] == 'S')
+                    start = (i, j);
+
+                map[i, j] = lines[i][j];
+            }
+        }
+        
+        var result = Day7.CalculateNumberOfSplits(map, start);
+
+        result.Should().Be(21);
+    }
+    
+    [Fact]
+    public void CalculateNumberOfSplits_WhenPart1_ReturnsCorrectCount()
+    {
+        var input = File.ReadAllText("Inputs/Day7/Day7.txt");
+
+        var lines = input.Split(Environment.NewLine);
+        var rows = lines.Length;
+        var cols = lines[0].Length;
+
+        var start = (0, 0);
+        var map = new char[rows, cols];
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (lines[i][j] == 'S')
+                    start = (i, j);
+
+                map[i, j] = lines[i][j];
+            }
+        }
+        
+        var result = Day7.CalculateNumberOfSplits(map, start);
+
+        result.Should().Be(21);
+    }
+}
