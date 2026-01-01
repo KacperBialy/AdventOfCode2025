a73c7ca9933b07b2dda22121f88a8f1d64bc0c7a
Kacper Bialy <kacper1400@o2.pl>
2026-01-01 20:13:38 +0100

    Day9 - Part 1

---
+++ AoC2025/Day9.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace AoC2025;
+
+public class Day9
+{
+    public static long CalculateMaxSquareArea(Point[] points)
+    {
+        long distance = 0;
+        for (var i = 0; i < points.Length; i++)
+        {
+            for (var j = i + 1; j < points.Length; j++)
+            {
+                if (i == j)
+                    continue;
+
+                var newDistance = (1L + Math.Abs(points[i].X - points[j].X))
+                                  * (1 + Math.Abs(points[i].Y - points[j].Y));
+
+                if (newDistance > distance)
+                    distance = newDistance;
+            }
+        }
+
+        return distance;
+    }
+}
---
+++ AoC2025Tests/Day9Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using AoC2025;
+using FluentAssertions;
+
+namespace TestProject1;
+
+public class Day9Tests
+{
+    [Fact]
+    public void CalculateMaxSquare_WhenSimpleInput_ReturnsCorrectMax()
+    {
+        var input = new Point[]
+        {
+            new() { X = 7, Y = 1 },
+            new() { X = 11, Y = 1 },
+            new() { X = 11, Y = 7 },
+            new() { X = 9, Y = 7 },
+            new() { X = 9, Y = 5 },
+            new() { X = 2, Y = 5 },
+            new() { X = 2, Y = 3 },
+            new() { X = 7, Y = 3 }
+        };
+
+        var maxSquare = Day9.CalculateMaxSquareArea(input);
+
+        maxSquare.Should().Be(50);
+    }
+
+    [Fact]
+    public void CalculateMaxSquare_WhenPart1Input_ReturnsCorrectMax()
+    {
+        var input = File.ReadAllLines("Inputs/Day9/Day9.txt")
+            .Select(row => row.Split(","))
+            .Select(pair => new Point(int.Parse(pair[0]), int.Parse(pair[1])))
+            .ToArray();
+
+        var maxSquare = Day9.CalculateMaxSquareArea(input);
+
+        maxSquare.Should().Be(4748826374);
+    }
+}
